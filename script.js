d5e9315b1539a431caafbb7cc416976e58b2c887
mattdavidsondesign <72038044+mattdavidsondesign@users.noreply.github.com>
2020-11-01 20:29:28 -0800

    Week 5 Videos and Project 1 Wrapup

---
+++ script.js
@@ -0,0 +1,54 @@
+$('.autoplay').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+});
+
+$('.single-slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    infinite: true,
+    dots: true,
+    draggable: false
+})
+
+$('.my-slick').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                centerMode: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                centerMode: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                centerMode: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+})
