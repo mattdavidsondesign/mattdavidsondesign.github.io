ec6668b99b964513bd2ecec1c1c59b132e7ee726
mattdavidsondesign <72038044+mattdavidsondesign@users.noreply.github.com>
2020-11-24 09:51:35 -0800

    Project 1 Meta

--- project2/script.js
+++ project2/script.js
@@ -37,6 +37,44 @@ j$(function(){
    );
 });
 
+$('.my-slick').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+})
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
