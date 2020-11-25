ab4c5b171627a81b696adb74bd458c594a9bbd10
mattdavidsondesign <72038044+mattdavidsondesign@users.noreply.github.com>
2020-11-25 13:38:25 -0800

    Nav Styling and Nav Script Attempt at Fix

--- project2/script.js
+++ project2/script.js
@@ -12,3 +12,41 @@ for (i = 0; i < coll.length; i++) {
     }
   })
 }
+var j$ = jQuery,
+    $nav = j$("#navigation"),
+    $slideLine = j$("#slide-line"),
+    $currentItem = j$(".current-item");
+
+j$(function(){  
+  // Menu has active item
+  if ($currentItem[0]) {
+    $slideLine.css({
+      "width": $currentItem.width(),
+      "left": $currentItem.position().left
+    });
+  }
+  
+  // Underline transition
+  j$($nav).find("li").hover(
+    // Hover on
+    function(){
+      $slideLine.css({
+        "width": j$(this).width(),
+        "left": j$(this).position().left
+      });
+    },
+    // Hover out
+    function(){
+      if ($currentItem[0]) {
+        // Go back to current
+        $slideLine.css({
+          "width": $currentItem.width(),
+          "left": $currentItem.position().left
+        });
+      } else {
+        // Disapear
+        $slideLine.width(0);
+      }
+    }
+   );
+});
