2cc6156a6c03a2d18db3731387bfcdfbc12ff1a3
alannarisse <angrypirate@gmail.com>
2020-11-25 10:24:42 -0800

    added javascript fixes

---
+++ project2/myslideshow.js
@@ -0,0 +1,9 @@
+$(".single-slide").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3500,
+  infinite: true,
+  dots: true,
+  draggable: false
+})
--- project2/script.js
+++ project2/script.js
@@ -1,91 +1,14 @@
-var j$ = jQuery,
-    $nav = j$("#navigation"),
-    $slideLine = j$("#slide-line"),
-    $currentItem = j$(".current-item");
-
-j$(function(){  
-  // Menu has active item
-  if ($currentItem[0]) {
-    $slideLine.css({
-      "width": $currentItem.width(),
-      "left": $currentItem.position().left
-    });
-  }
-  
-  // Underline transition
-  j$($nav).find("li").hover(
-    // Hover on
-    function(){
-      $slideLine.css({
-        "width": j$(this).width(),
-        "left": j$(this).position().left
-      });
-    },
-    // Hover out
-    function(){
-      if ($currentItem[0]) {
-        // Go back to current
-        $slideLine.css({
-          "width": $currentItem.width(),
-          "left": $currentItem.position().left
-        });
-      } else {
-        // Disapear
-        $slideLine.width(0);
-      }
-    }
-   );
-});
-
-$('.my-slick').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2500,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        centerMode: true,
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-})
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
+var coll = document.getElementsByClassName("collapsible")
+var i
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active")
+    var content = this.nextElementSibling
     if (content.style.display === "block") {
-      content.style.display = "none";
+      content.style.display = "none"
     } else {
-      content.style.display = "block";
+      content.style.display = "block"
     }
-  });
+  })
 }
